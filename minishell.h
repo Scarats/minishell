fc0da68e0118d11da4ee692b4fb0d46c19f8955a
Thomas.home <0xscarats@gmail.com>
2025-08-02 18:54:51 +0200

    New branch, thomas

--- minishell.h
+++ minishell.h
@@ -1,65 +1,59 @@
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include "./utils/libft/libft.h"
-# include <fcntl.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
+#include "./utils/libft/libft.h"
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-typedef enum e_node_type
-{
-    NODE_COMMAND,
-    NODE_PIPE,
-    NODE_AND,
-    NODE_OR
-}                    t_node_type;
+typedef enum e_node_type {
+  NODE_COMMAND,
+  NODE_PIPE,
+  NODE_AND,
+  NODE_OR
+} t_node_type;
 
-typedef struct s_input
-{
-	char			*data;
+typedef struct s_input {
+  char *data;
 
-	struct s_input	*next;
-	struct s_input	*previous;
-}					t_input;
+  struct s_input *next;
+  struct s_input *previous;
+} t_input;
 
-typedef struct s_output
-{
-	char			*data;
+typedef struct s_output {
+  char *data;
 
-	bool append; // for >>
-	struct s_output	*next;
-	struct s_output	*previous;
-}					t_output;
+  bool append; // for >>
+  struct s_output *next;
+  struct s_output *previous;
+} t_output;
 
-typedef struct s_cmd
-{
-	char			**token;
+typedef struct s_cmd {
+  char **tokens;
 
-	char			*function;
-	char			**options;
-	char *redirection; // Will change the node's output.
-	char *path;         // Can be a file or a directory (i guess lol)
-}					t_cmd;
+  char *function;
+  char **options;
+  char *redirection; // Will change the node's output.
+  char *path;        // Can be a file or a directory (i guess lol)
+} t_cmd;
 
-typedef struct s_node
-{
-	t_node_type		type;
+typedef struct s_node {
+  t_node_type type;
 
-	struct s_node	*left;
-	struct s_node	*right;
-	struct s_node	*parent;
+  struct s_node *left;
+  struct s_node *right;
+  struct s_node *parent;
 
-	t_cmd			*cmd;
-	t_input			*input;
-	t_output		*output;
-}					t_node;
+  t_cmd *cmd;
+  t_input *input;
+  t_output *output;
+} t_node;
 
-typedef struct s_main_data
-{
-	t_node **
-}					t_main_data;
+typedef struct s_main_data {
+  t_node *node;
+} t_main_data;
 
 #endif
