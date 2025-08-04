76ee63ae0c44fffb9fcdc439b6ac7c313c9801c6
Thomas.macbook <0xscarats@gmail.com>
2025-08-04 17:39:55 +0200

    WIP.
    Working on the execution of the binary tree.
    
    Working on multi piping to start.

---
+++ src/execution/execution.c
@@ -0,0 +1,15 @@
+#include "../minishell.h"
+
+// Will check the type of the node.
+// Then redirect to the according type.
+// It will execute recursively.
+int execute_node(t_node	*node, int input_fd, int output_fd)
+{
+	// 1. Check type.
+	if (node->type == NODE_COMMAND)
+	if (node->type == NODE_PIPE)
+	if (node->type == NODE_AND)
+	if (node->type == NODE_OR)
+	// 		
+	// 2. Call type function.
+}
