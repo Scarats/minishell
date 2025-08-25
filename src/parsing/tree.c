b527e86e1bdf2a51647518516aa3353e5e3636de
Thomas.42 <0xscarats@gmail.com>
2025-08-25 14:16:52 +0200

    WIP build tree (AST) : To navigate through the tokens, why not making an array of ptr to token ?
    So everytime you can easily split the the array in two.
    
    Left part = arr[0] → arr[prev operator]
    
    Right part = arr[prev operator] → arr[n]

---
+++ src/parsing/tree.c
@@ -0,0 +1,10 @@
+#include "../../minishell.h"
+
+// make a recursive function that will find the most significant operator.
+// create a node from it.
+// Call itself from the first token of the left part, and call itself from the first token of the right part.
+// Continue until no operator is found.
+int build_tree()
+{
+
+}
