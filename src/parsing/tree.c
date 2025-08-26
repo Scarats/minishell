cd54134f2fe1c3fe9ae8ecce84719d7b4772e0bb
Thomas.42 <0xscarats@gmail.com>
2025-08-26 16:21:07 +0200

    WIP, see chat for errors in logic.

--- src/parsing/tree.c
+++ src/parsing/tree.c
@@ -1,8 +1,8 @@
 #include "../../minishell.h"
 
-int set_file(t_main_data *data, t_node *node, t_token *token)
+int add_file(t_main_data *data, t_node *node, t_token *token)
 {
-	if (token->type != TOKEN_FILE)
+	if (token->type != TOKEN_REDIRECT_OUT && token->type != TOKEN_REDIRECT_IN && token->type != TOKEN_APPEND && token->type != TOKEN_HEREDOC)
 		return (0);
 	else if (token->type == TOKEN_REDIRECT_OUT)
 	{
@@ -21,61 +21,59 @@ int create_node_cmd(t_main_data *data, t_token *tok_list, t_node *node, int size
 {
 	int i;
 	int cmd_toks;
-	
+
 	i = -1;
 	cmd_toks = 0;
 	while (++i < size)
 	{
-		if (&tok_list[i].type == TOKEN_CMD || tok_list[i].type == TOKEN_ARGUMENT)
+		if (tok_list[i].type == TOKEN_CMD || tok_list[i].type == TOKEN_ARGUMENT)
 			cmd_toks++;
-		else
+		else if (cmd_toks > 0)
 		{
-			if (cmd_toks > 0)
+			node->cmd->tokens = my_malloc(data->malloc_tree, sizeof(char *) * cmd_toks);
+			while (cmd_toks > 0)
 			{
-				node->cmd->tokens = my_malloc(data->malloc_tree, sizeof(char *) * cmd_toks);
-				while (cmd_toks > 0)
-				{
-					node->cmd->tokens[cmd_toks--] = ft_strdup(&tok_list[cmd_toks].word);
-					my_addtolist(data->malloc_tree, node->cmd->tokens[cmd_toks]);
-				}
-			}	
+				node->cmd->tokens[cmd_toks--] = ft_strdup(&tok_list[cmd_toks].word);
+				my_addtolist(data->malloc_tree, node->cmd->tokens[cmd_toks]);
+			}
 		}
-		if (&tok_list[i].type == TOKEN_REDIRECT_IN || TOKEN_REDIRECT_OUT || TOKEN_APPEND || TOKEN_HEREDOC)
+		if (tok_list[i].type == TOKEN_REDIRECT_IN || TOKEN_REDIRECT_OUT || TOKEN_APPEND || TOKEN_HEREDOC)
 			node->redirection = &tok_list[i].type;
-		set_file(data, node, &tok_list[i]); 
+		add_file(data, node, &tok_list[i]);
 	}
+	return (0);
 }
 
-
 // While size, add token, check its type and add it to the node.
-t_node *create_node(t_main_data *data, t_token *tok_list, t_node_type type, int size)
+t_node *create_node(t_main_data *data, t_token *tok_list, t_node_type type,
+					int size)
 {
 	t_node *node;
 
 	node = my_malloc(data->malloc_tree, sizeof(t_node));
-	ft_memset(&node, 0, sizeof(node));
+	ft_memset(node, 0, sizeof(node));
 	node->type = type;
 	if (type != NODE_COMMAND)
 		return (&node);
 	create_node_cmd(data, tok_list, node, size);
-	return (&node);
+	return (node);
 }
 
 // Return the index of the most significant operator.
-// If none found, return -1;
+// If none found, return (-1);
 int find_operator(t_token *tok_list, int size)
 {
-
 }
 
 // make a recursive function that will find the most significant operator.
 // create a node from it.
 // Call itself from the first token of the left part, and call itself from the first token of the right part.
 // Continue until no operator is found.
-int build_tree(t_main_data *data, t_node *node, t_token *tok_list, int size)
+t_node *build_tree(t_main_data *data, t_token *tok_list, int size)
 {
 	int i;
 	char **cmd;
+	t_node *node;
 
 	cmd = NULL;
 	i = find_operator(tok_list, size);
@@ -88,15 +86,16 @@ int build_tree(t_main_data *data, t_node *node, t_token *tok_list, int size)
 			cmd[i] = ft_strdup(tok_list[i].word);
 		my_array_addtolist(data->malloc_tree, cmd);
 		node = create_node(tok_list[0].type, cmd, size);
-		return (0);
+		return (node);
 	}
 	else
 	{
+		// Create the new node in the call of each side.
 		node = create_node(tok_list[i].type, tok_list[i].word, size);
-		// create new_node at tok_list[i]
-		// Left
-		if (build_tree(data, node->left, &tok_list[0], i) || build_tree(data, node->right, &tok_list[i], size - i))
+
+		node->left = build_tree(data, &tok_list[0], i);
+		node->right = build_tree(data, &tok_list[i], size - i))
 			return (1);
-		return (0);
+		return (node);
 	}
 }
