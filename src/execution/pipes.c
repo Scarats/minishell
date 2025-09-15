218e24b3a53f586ef9757f3c8d68152ec60c6f4a
Thomas.42 <0xscarats@gmail.com>
2025-09-15 14:49:25 +0200

    Fixed the pipe execution issue.

--- src/execution/pipes.c
+++ src/execution/pipes.c
@@ -3,61 +3,79 @@
 // Will call the left node.
 int left(t_node *node, t_main_data *data)
 {
-	int error;
+    int error;
 
-	error = 0;
-	close(node->pipefd[0]);
-	data->in_child = true;
-	error = traverse_tree(node->left, data);
-	exit(error);
+    error = 0;
+    close(node->pipefd[0]);
+
+    // Make pipe the default stdout in this child first
+    if (dup2(node->pipefd[1], STDOUT_FILENO) == -1)
+        exit(1);
+    close(node->pipefd[1]);
+
+    // Pass parent's input fd into the left subtree so set_io_fds can use it
+    if (node->input_fd != -1)
+        node->left->input_fd = node->input_fd;
+    // ensure not to pre-set left->output_fd here; pipe is already set via dup2
+
+    data->in_child = true;
+    error = traverse_tree(node->left, data);
+    exit(error);
 }
 
 // Will call the right node.
 int right(t_node *node, t_main_data *data)
 {
-	int error;
+    int error;
 
-	error = 0;
-	close(node->pipefd[1]);
-	data->in_child = true;
-	error = traverse_tree(node->right, data);
-	exit(error);
+    error = 0;
+    close(node->pipefd[1]);
+
+    // Make pipe the default stdin in this child first
+    if (dup2(node->pipefd[0], STDIN_FILENO) == -1)
+        exit(1);
+    close(node->pipefd[0]);
+
+    // Pass parent's output fd into the right subtree so set_io_fds can use it
+    if (node->output_fd != -1)
+        node->right->output_fd = node->output_fd;
+    // ensure not to pre-set right->input_fd here; pipe is already set via dup2
+
+    data->in_child = true;
+    error = traverse_tree(node->right, data);
+    exit(error);
 }
 
 // Will create two childs, left and right, for each end of the pipe.
 int pipes(t_node *node, t_main_data *data)
 {
-	int status_left;
-	int status_right;
+    int status_left;
+    int status_right;
 
-	if (pipe(node->pipefd) == -1)
-		return (perror("pipe"), 1);
+    if (pipe(node->pipefd) == -1)
+        return (perror("pipe"), 1);
 
-	node->left->input_fd = node->input_fd;
-	node->left->output_fd = node->pipefd[1];
-	node->right->input_fd = node->pipefd[0];
-	node->right->output_fd = node->output_fd;
+    node->left_pid = fork();
+    if (node->left_pid == -1)
+        return (perror("fork"), 1);
+    else if (node->left_pid == 0)
+        left(node, data);
 
-	node->left_pid = fork();
-	if (node->left_pid == -1)
-		return (perror("fork"), 1);
-	else if (node->left_pid == 0)
-		left(node, data);
+    node->right_pid = fork();
+    if (node->right_pid == -1)
+        return (perror("fork"), 1);
+    else if (node->right_pid == 0)
+        right(node, data);
 
-	node->right_pid = fork();
-	if (node->right_pid == -1)
-		return (perror("fork"), 1);
-	else if (node->right_pid == 0)
-		right(node, data);
+    close(node->pipefd[0]);
+    close(node->pipefd[1]);
 
-	close(node->pipefd[0]);
-	close(node->pipefd[1]);
-	if (waitpid(node->left_pid, &status_left, 0) == -1 || waitpid(node->right_pid, &status_right, 0) == -1)
-		return (1);
+    if (waitpid(node->left_pid, &status_left, 0) == -1 || waitpid(node->right_pid, &status_right, 0) == -1)
+        return (1);
 
-	if (WIFEXITED(status_right))
-		return (WEXITSTATUS(status_right));
-	if (WIFSIGNALED(status_right))
-		return (128 + WTERMSIG(status_right));
-	return (0);
+    if (WIFEXITED(status_right))
+        return (WEXITSTATUS(status_right));
+    if (WIFSIGNALED(status_right))
+        return (128 + WTERMSIG(status_right));
+    return (0);
 }
