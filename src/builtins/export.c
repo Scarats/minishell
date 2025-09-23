5e3c1ad89066c0b6cc90013b36b2a1f05cd03d1d
Thomas.home.VM <0xscarats@gmail.com>
2025-09-23 23:30:02 +0200

    export working.

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,16 +3,19 @@
 int export(t_root *root, char *var)
 {
 	t_env *new_var;
+	t_env *ptr;
 	char **args;
 
 	if (!root)
 		return (1);
 	args = NULL;
 	new_var = my_malloc(&root->malloc_root, sizeof(t_env));
-	while (root->env->next != NULL)
-		root->env = root->env->next;
-	root->env->next = new_var;
+	ptr = root->env;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = new_var;
 	args = ft_split(var, '=');
+	my_array_addtolist(&root->malloc_root, (void **)args);
 	new_var->exported = true;
 	new_var->name = args[0];
 	new_var->value = args[1];
--- src/execution/exec_builtins.c
+++ src/execution/exec_builtins.c
@@ -21,7 +21,7 @@ int exec_builtins(t_node *node, t_main_data *data)
 	else if (!ft_strncmp(node->cmd_argv[0], "matrix", lenght))
 		error = matrix(node->cmd_argv);
 	else if (!ft_strncmp(node->cmd_argv[0], "export", lenght))
-		error = export(data->root, node->cmd_argv[0]);
+		error = export(data->root, node->cmd_argv[1]);
 	else
 		printf(RED "error: built-in not found.\n" RESET);
 	return (error);
