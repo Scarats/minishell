698558bcd668d4d4fc8429e780186aa36e29c4e2
Thomas.42 <0xscarats@gmail.com>
2025-11-17 18:54:35 +0100

    Working version, to be tested.

--- src/execution/pipes.c
+++ src/execution/pipes.c
@@ -6,7 +6,7 @@
 /*   By: tcardair <tcardair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 14:55:44 by tcardair          #+#    #+#             */
-/*   Updated: 2025/11/17 18:37:36 by tcardair         ###   ########.fr       */
+/*   Updated: 2025/11/17 18:52:33 by tcardair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,31 @@
 // Will call the left node.
 int	left(t_node *node, t_main_data *data)
 {
-	int		error;
-	t_root	*root;
+    int	error;
 
-	root = data->root;
-	error = 0;
-	node->pipe_left = 1;
-	// close(node->pipefd[0]);
-	node->output_fd = node->pipefd[1];
-	if (node->input_fd != -1)
-		node->left->input_fd = node->input_fd;
-	error = traverse_tree(node->left, data);
-	// close(node->pipefd[1]);
-	// my_multi_free(&root->list_of_list);
-	return (error);
+    error = 0;
+    node->pipe_left = 1;
+    node->left->output_fd = node->pipefd[1];
+    node->pipefd[1] = -1;
+    if (node->input_fd != -1)
+        node->left->input_fd = node->input_fd;
+    error = traverse_tree(node->left, data);
+    return (error);
 }
 
 // Will call the right node.
 int	right(t_node *node, t_main_data *data)
 {
-	int		error;
-	t_root	*root;
+    int	error;
 
-	root = data->root;
-	error = 0;
-	node->pipe_right = 1;
-	// close(node->pipefd[1]);
-	node->input_fd = node->pipefd[0];
-	if (node->output_fd != -1)
-		node->right->output_fd = node->output_fd;
-	error = traverse_tree(node->right, data);
-	// close(node->pipefd[0]);
-	// my_multi_free(&root->list_of_list);
-	return (error);
+    error = 0;
+    node->pipe_right = 1;
+    node->right->input_fd = node->pipefd[0];
+    node->pipefd[0] = -1;
+    if (node->output_fd != -1)
+        node->right->output_fd = node->output_fd;
+    error = traverse_tree(node->right, data);
+    return (error);
 }
 
 int	pipes_logic(t_node *node, t_main_data *data)
@@ -73,20 +65,21 @@ int	pipes_logic(t_node *node, t_main_data *data)
 // Will create two childs, left and right, for each end of the pipe.
 int	pipes(t_node *node, t_main_data *data)
 {
-	t_root	*root;
-	int error;
+    int	error;
 
-	node->in_pipe = true;
-	error = 0;
-	if (!node || !data)
-		return (1);
-	root = data->root;
-	error = pipes_logic(node, data);
-	if (node->pipefd[0] != -1)
+    error = 0;
+    if (!node || !data)
+        return (1);
+    if (pipe(node->pipefd) == -1)
+        return (perror("pipe"), 1);
+    error = left(node, data);
+    if (error == 0)
+        error = right(node, data);
+    if (node->pipefd[0] != -1)
         close(node->pipefd[0]);
     if (node->pipefd[1] != -1)
         close(node->pipefd[1]);
     node->pipefd[0] = -1;
     node->pipefd[1] = -1;
-	return (error);
+    return (error);
 }
--- src/handlers/redirections.c
+++ src/handlers/redirections.c
@@ -6,7 +6,7 @@
 /*   By: tcardair <tcardair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 14:42:15 by tcardair          #+#    #+#             */
-/*   Updated: 2025/11/14 20:00:20 by tcardair         ###   ########.fr       */
+/*   Updated: 2025/11/17 18:53:57 by tcardair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,17 +107,20 @@ int	redirections(t_node *node, t_main_data *data)
 
 int	set_io_fds(t_node *node, t_main_data *data)
 {
-	if (!data)
-		data = NULL;
-	if (node->input_fd != -1)
-	{
-		dup2(node->input_fd, STDIN_FILENO);
-		close(node->input_fd);
-	}
-	if (node->output_fd != -1)
-	{
-		dup2(node->output_fd, STDOUT_FILENO);
-		close(node->output_fd);
-	}
-	return (0);
+    (void)data;
+    if (node->input_fd != -1 && node->input_fd != STDIN_FILENO)
+    {
+        if (dup2(node->input_fd, STDIN_FILENO) == -1)
+            return (errno);
+        close(node->input_fd);
+        node->input_fd = STDIN_FILENO;
+    }
+    if (node->output_fd != -1 && node->output_fd != STDOUT_FILENO)
+    {
+        if (dup2(node->output_fd, STDOUT_FILENO) == -1)
+            return (errno);
+        close(node->output_fd);
+        node->output_fd = STDOUT_FILENO;
+    }
+    return (0);
 }
